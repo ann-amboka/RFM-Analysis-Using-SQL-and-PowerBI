3af6d7afba1c7f1cddbb52fd2498c7f2acd9bcce
Ann Amboka (Anna) <amukasahann@gmail.com>
2026-04-20 11:29:07 +0300

    Update View.sql

--- SCRIPTS/View.sql
+++ SCRIPTS/View.sql
@@ -1,15 +1,61 @@
--- ============================================================
--- CREATE CLEAN VIEW
--- ============================================================
 
-CREATE VIEW vw_Online_Retail_Clean AS
+--===================================================================--
+---------------------CREATE VIEW FOR JOINS----------------------------
+CREATE VIEW vw_RFM AS
+
 SELECT
-    Invoice,
-    ISNULL(StockCode,                        'N/A') AS StockCode,
-    ISNULL(Description,                      'N/A') AS Description,
-    Quantity,
-    ISNULL(CONVERT(VARCHAR, InvoiceDate, 120),'N/A') AS InvoiceDate,
-    ISNULL(CAST(Price AS VARCHAR),           '0'  ) AS Price,
-    ISNULL(CustomerID,                       'N/A') AS CustomerID,
-    ISNULL(Country,                          'N/A') AS Country
-FROM Online_Retail;
+customer_id,
+RFMScore,
+segment,
+Recency,
+Frequency,
+Monetary
+FROM
+(
+SELECT
+customer_id,
+Recency,
+Frequency,
+Monetary,
+RFMScore,
+CASE
+        WHEN RFMScore >= 12 THEN 'Champions'
+        WHEN RFMScore >= 9 THEN 'Loyal'
+        WHEN RFMScore >= 6 THEN 'Potential'
+        ELSE 'At Risk'
+    END AS segment
+FROM
+(
+SELECT
+customer_id,
+Recency,
+Frequency,
+Monetary,
+recency_score,
+frequency_score,
+monetary_score,
+recency_score+frequency_score+monetary_score AS RFMScore
+FROM
+(
+	SELECT
+		customer_id,
+		Recency,
+		Frequency,
+		Monetary,
+		NTILE(5) OVER (ORDER BY Recency) AS recency_score,
+		NTILE(5) OVER (ORDER BY Frequency) AS frequency_score,
+		NTILE(5) OVER (ORDER BY Monetary) AS monetary_score
+	FROM
+(
+	SELECT
+	customer_id,
+	DATEDIFF(day,MAX(transaction_date), GETDATE()) AS Recency,
+	COUNT(order_id) AS Frequency,
+	SUM(total_amount_usd) AS Monetary
+	FROM Transactions
+	WHERE order_status = 'Completed'
+	GROUP  BY customer_id
+)T
+)T
+)T
+)t
